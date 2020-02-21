49490f1059d66f6c2e790c58eebe13c6b3446607
wxiangbei <1526768597@qq.com>
2020-02-22 00:04:10 +0800

    init vue-admin

---
+++ src/views/Login/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="login">
+    hjkl <el-button>rtgtyuk</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='scss' scoped>
+#login {
+  background: #344a5f;
+  height: 100vh;
+}
+</style>
